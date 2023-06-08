fdd4a0276739bda57fe18424165b983bc03a6bd5
Aaron Miller <drkstr101@gmail.com>
2023-06-08 23:43:00 +0000

    add form validation
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# Article model
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
