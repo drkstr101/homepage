b9b52a45d159842155c1680087498ee0cd41b043
Aaron Miller <drkstr101@gmail.com>
2023-06-08 23:41:12 +0000

    add new article form
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Article controller
+# ArticlesController handles primary page routes
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -8,5 +8,25 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
