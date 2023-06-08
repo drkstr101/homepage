405f82b52769c5f89cf6914361f7cd608c17117c
Aaron Miller <drkstr101@gmail.com>
2023-06-08 23:34:18 +0000

    setup show article route
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
+# Article controller
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
