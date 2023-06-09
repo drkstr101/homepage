8eba4d0d6ac39533f6dbf01e24c7307301b66121
Aaron Miller <drkstr101@gmail.com>
2023-06-09 21:24:15 +0000

    add visible concern
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,7 @@
 
 # Comment model belongs to page
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  VALID_STATES = %w[public private archived].freeze
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
