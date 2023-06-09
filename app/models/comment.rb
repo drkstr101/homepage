e756621a28270e688063508851a342bef45dcf8e
Aaron Miller <drkstr101@gmail.com>
2023-06-09 19:25:53 +0000

    impl status field
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@
 # Comment model belongs to page
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALID_STATES = %w[public private archived].freeze
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
