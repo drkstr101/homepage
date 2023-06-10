e856ad03042cfd4de57211437499284b9fbfa57d
Aaron Miller <drkstr101@gmail.com>
2023-06-10 18:47:37 +0000

    add daisy ui
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -5,5 +7,81 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
   ],
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('daisyui'), require('@tailwindcss/forms')],
+  daisyui: {
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
+   theme: {
+    fontSize: {
+      xs: ['0.875rem', { lineHeight: '1.5rem' }],
+      sm: ['1rem', { lineHeight: '1.5rem' }],
+      base: ['1.125rem', { lineHeight: '2rem' }],
+      lg: ['1.25rem', { lineHeight: '1.75rem' }],
+      xl: ['1.5rem', { lineHeight: '2rem' }],
+      '2xl': ['2rem', { lineHeight: '3rem' }],
+      '3xl': ['2.5rem', { lineHeight: '3rem' }],
+      '4xl': ['3rem', { lineHeight: '1' }],
+      '5xl': ['3.75rem', { lineHeight: '1' }],
+      '6xl': ['4.5rem', { lineHeight: '1' }],
+      '7xl': ['6rem', { lineHeight: '1' }],
+      '8xl': ['8rem', { lineHeight: '1' }],
+    },
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 0.5s linear forwards',
+        marquee: 'marquee var(--marquee-duration) linear infinite',
+        'spin-slow': 'spin 4s linear infinite',
+        'spin-slower': 'spin 6s linear infinite',
+        'spin-reverse': 'spin-reverse 1s linear infinite',
+        'spin-reverse-slow': 'spin-reverse 4s linear infinite',
+        'spin-reverse-slower': 'spin-reverse 6s linear infinite',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+      },
+      colors: ({ colors }) => ({
+        primary: colors.cyan,
+        secondary: colors.sky,
+        accent: colors.teal,
+        white: '#fafafa',
+        black: '#0a0a0a',
+      }),
+      fontFamily: {
+        sans: ['Fira Sans', ...fontFamily.sans],
+        mono: ['Fira Code', ...fontFamily.mono],
+        condensed: ['Fira Sans Condensed', ...fontFamily.sans],
+      },
+      keyframes: {
+        'fade-in': {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+        marquee: {
+          '100%': {
+            transform: 'translateY(-50%)',
+          },
+        },
+        'spin-reverse': {
+          to: {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      maxWidth: {
+        '2xl': '40rem',
+      },
+    },
+  },
 };
