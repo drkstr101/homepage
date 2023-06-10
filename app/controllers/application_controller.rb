6d15ac813af8d7a1140a87a34832c5cbd918782b
Aaron Miller <drkstr101@gmail.com>
2023-06-10 07:52:11 +0000

    set application index route
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# ApplicationController - top level page router
 class ApplicationController < ActionController::Base
+  def index
+    @articles = Article.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'articles#index'
+  root 'application#index'
 
-  resources :articles do
-    resources :comments
-  end
+  # resources :articles do
+  #   resources :comments
+  # end
 end
