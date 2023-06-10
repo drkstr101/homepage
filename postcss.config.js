db0f24df0162f5269d08acec7f311405f5ee04ee
Aaron Miller <drkstr101@gmail.com>
2023-06-10 18:47:37 +0000

    add daisy ui
    Signed-off-by: Aaron Miller <drkstr101@gmail.com>

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
