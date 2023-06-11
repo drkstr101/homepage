411323538c7b378ba44a46620ef6549b8c883c92
Aaron R Miller <amiller@watheia.com>
2023-06-11 11:22:06 -0700

    generate posts model
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_611_050_215) do
+ActiveRecord::Schema[7.0].define(version: 20_230_611_181_027) do
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.string 'branch'
+    t.integer 'posts_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['posts_id'], name: 'index_categories_on_posts_id'
+  end
+
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.integer 'user_id', null: false
+    t.integer 'category_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_posts_on_category_id'
+    t.index ['user_id'], name: 'index_posts_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -25,4 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 20_230_611_050_215) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'categories', 'posts', column: 'posts_id'
+  add_foreign_key 'posts', 'categories'
+  add_foreign_key 'posts', 'users'
 end
