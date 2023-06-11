512739d1cacf2bd81113fef4628d190a57aec2c6
Aaron R Miller <amiller@watheia.com>
2023-06-10 22:41:08 -0700

    generate device controllers
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# User - Represents the authenticated principal user
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'pages#index'
+
+  # Setup user routes
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
