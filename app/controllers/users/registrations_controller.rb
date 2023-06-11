e50925714262c7325573cd89870c47a6df50991f
Aaron R Miller <amiller@watheia.com>
2023-06-11 01:35:29 -0700

    update allowed registration params
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Users
+  # RegistrationsController - handle routes for user signup
   class RegistrationsController < Devise::RegistrationsController
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
@@ -39,17 +40,18 @@ module Users
     #   super
     # end
 
-    # protected
+    protected
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_up_params
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-    # end
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    end
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-    # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update,
+                                        keys: %i[name email password password_confirmation current_password])
+    end
 
     # The path used after sign up.
     # def after_sign_up_path_for(resource)
