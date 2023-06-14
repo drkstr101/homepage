046689833bdc9a1d3d9812e2d6975a89feaede34
Aaron R Miller <amiller@watheia.com>
2023-06-13 21:05:02 -0700

    fix header nav
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,4 +2,11 @@
 
 # helper methods for page navigation
 module NavigationHelper
+  def show_login?
+    !user_signed_in? \
+      && controller_name != 'sessions' \
+      && controller_name != 'registrations' \
+      && controller_name != 'passwords' \
+      && controller_name != 'confirmations'
+  end
 end
