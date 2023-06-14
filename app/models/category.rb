3003ec2c8c168a58f4429c724e59f75c0fc374d5
Aaron R Miller <amiller@watheia.com>
2023-06-13 18:48:49 -0700

    remove category model
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- app/models/category.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# Category - top level classification for posts
-class Category < ApplicationRecord
-  belongs_to :posts
-end
--- db/migrate/20230611180905_create_posts.rb
+++ db/migrate/20230611180905_create_posts.rb
@@ -6,8 +6,6 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :content
       t.belongs_to :user, null: false, foreign_key: true, index: true
-      t.belongs_to :category, null: false, foreign_key: true, index: true
-
       t.timestamps
     end
   end
