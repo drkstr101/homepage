2b8ae6c2ce9bfcd9e5b5e6edca6eabaa294c9974
Aaron R Miller <amiller@watheia.com>
2023-07-03 12:06:19 -0700

    about_exceptions koans
    Signed-off-by: Aaron R Miller <amiller@watheia.com>

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -2,15 +2,16 @@
 
 require File.expand_path("#{File.dirname(__FILE__)}/neo")
 
+# AboutExceptions
 class AboutExceptions < Neo::Koan
   class MySpecialError < RuntimeError
   end
 
-  def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+  def test_exceptions_inherit_from_exception
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
   end
 
   def test_rescue_clause
@@ -21,15 +22,15 @@ class AboutExceptions < Neo::Koan
       result = :exception_handled
     end
 
-    assert_equal __, result
+    assert_equal :exception_handled, result
 
-    assert_equal __, e.is_a?(StandardError), 'Should be a Standard Error'
-    assert_equal __, e.is_a?(RuntimeError),  'Should be a Runtime Error'
+    assert_equal true, e.is_a?(StandardError), 'Should be a Standard Error'
+    assert_equal true, e.is_a?(RuntimeError), 'Should be a Runtime Error'
 
     assert RuntimeError.ancestors.include?(StandardError),
            'RuntimeError is a subclass of StandardError'
 
-    assert_equal __, e.message
+    assert_equal 'Oops', e.message
   end
 
   def test_raising_a_particular_error
@@ -41,8 +42,8 @@ class AboutExceptions < Neo::Koan
       result = :exception_handled
     end
 
-    assert_equal __, result
-    assert_equal __, e.message
+    assert_equal :exception_handled, result
+    assert_equal 'My Message', e.message
   end
 
   def test_ensure_clause
@@ -54,13 +55,13 @@ class AboutExceptions < Neo::Koan
       result = :always_run
     end
 
-    assert_equal __, result
+    assert_equal :always_run, result
   end
 
   # Sometimes, we must know about the unknown
   def test_asserting_an_error_is_raised
     # A do-end is a block, a topic to explore more later
-    assert_raise(___) do
+    assert_raise(MySpecialError) do
       raise MySpecialError, 'New instances can be raised directly.'
     end
   end
